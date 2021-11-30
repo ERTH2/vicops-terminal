5b6fce1c580ef858aaae8881108b3447f454f1da
Artemy <artegoser@gmail.com>
2021-11-30 21:02:46 +0300

    standart

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 const { vicopsApi } = require("vicops-api");
 const { Readline } = require("easy-readline");
+const utils = require("./utils");
 const fs = require("fs");
 let sr = new Readline();
-
+let user = new vicopsApi();
 handle();
 
 async function handle(){
-    let resp = await sr.input("text", "Введите команду: ");
+    let resp = await sr.input("text", ">>> ");
     resp = resp.toLowerCase();
 
     if(resp==="регистрация"){
-
+        user = new vicopsApi(await sr.input("text", "Введите логин: "), await sr.input("secure", "Введите пароль: "));
+        user.register(await sr.input("text", "Введите электронную почту для восстановления: "));
     } else if(resp==="войти"){
-        let user = new vicopsApi(await sr.input("text", "Введите логин: "), await sr.input("secure", "Введите пароль: "));
-        console.table((await user.getUser()).private.balances);
+        user = new vicopsApi(await sr.input("text", "Введите логин: "), await sr.input("secure", "Введите пароль: "));
+        let balances = (await user.getUser())?.private?.balances;
+        console.log("Активы:")
+        if(balances) utils.printBal(balances);
+        else console.log(await user.getUser());
+    } else if(resp==="помощь") {
+        utils.printHelp();
+    } else if(resp==="баланс"){
+        let balances = (await user.getUser())?.private?.balances;
+        if(balances) utils.printBal(balances);
+        else console.log(await user.getUser());
+    } else if(resp==="история"){
+        let balances = (await user.getUser())?.private?.transactions;
+        if(balances) utils.printTrans(balances, user.name);
+        else console.log(await user.getUser());
+    } else if(resp==="транзакция"){
+        let transaction = await user.transaction(await sr.input("text", "Получатель: "), Number(await sr.input("text", "Количество: ")), await sr.input("text", "Валюта: "), await sr.input("text", "Комментарий: "));
+        console.log(transaction);
+    } else if(resp==="курс"){
+        let name = await sr.input("text", "Название котировки: ");
+        console.log("Курс ")
     }
+    handle();
 }
---
+++ utils.js
@@ -0,0 +1,17 @@
+module.exports = {
+    printBal:(balances)=>{
+        let curr = Object.keys(balances);
+        let vals = Object.values(balances);
+        for(let i=0; i<curr.length; i++) console.log(`${vals[i]} ${curr[i]}`);
+    },
+    printHelp:()=>{
+        let help = ["регистрация", "транзакция", "история", "баланс", "обмен", "курс", "помощь"]
+        for(let h of help) console.log(h);
+    },
+    printTrans:(transactions, name)=>{
+        for(let transaction of transactions){
+            if(transaction.body.recipient===name) console.log(`+ ${transaction.body.amount.toFixed(5)} ${transaction.body.currency} от ${transaction.body.sender}. Комментарий: ${transaction.body.comment}`);
+            else console.log(`- ${transaction.body.amount.toFixed(5)} ${transaction.body.currency} отправлено ${transaction.body.recipient}. Комментарий: ${transaction.body.comment}`);
+        }
+    }
+}
