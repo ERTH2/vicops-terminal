def186dea0eaa24c8cc505ccaf3b7ae0dbdcf8d9
Artemy <artegoser@gmail.com>
2022-03-06 21:40:06 +0300

    yes

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const { vicopsApi } = require("vicops-api");
 const { Readline } = require("easy-readline");
 const utils = require("./utils");
 const fs = require("fs");
+const path = require("path");
 let sr = new Readline();
+
+const LocalStorage = require("node-localstorage").LocalStorage;
+
+const tpath = path.join(process.env.APPDATA, "vicopsTerm");
+
+if (!fs.existsSync(tpath)) {
+  fs.mkdirSync(tpath);
+}
+
+let ls = new LocalStorage(tpath);
 
 const CFonts = require("cfonts");
 
@@ -17,7 +28,7 @@ CFonts.say("VICOPS|terminal v1.2.0", {
 
 let user;
 
-start();
+login();
 handle();
 
 async function handle() {
@@ -25,48 +36,68 @@ async function handle() {
   resp = resp.toLowerCase();
 
   if (resp === "регистрация") {
-    user = new vicopsApi(
-      await sr.input("text", "Введите логин: "),
-      await sr.input("secure", "Введите пароль: ")
-    );
-    let response = await user.register(
-      await sr.input("text", "Введите электронную почту для восстановления: ")
-    );
-    console.log(response);
+    let ulogin = await sr.input("text", "Введите логин: ");
+    let fpass = await sr.input("secure", "Введите пароль: ");
+    let spass = await sr.input("secure", "Введите пароль еще раз: ");
+    if (spass === fpass) {
+      user = new vicopsApi(ulogin, fpass);
+      let response = await user.register(
+        await sr.input("text", "Введите электронную почту для восстановления: ")
+      );
+      console.log(response);
+      ls.setItem("login", ulogin);
+      ls.setItem("password", fpass);
+    } else console.log("Пароли не совпадают".red);
+  } else if (resp === "войти") {
+    let ulogin = await sr.input("text", "Введите логин: ");
+    let pass = await sr.input("secure", "Введите пароль: ");
+    user = new vicopsApi(ulogin, pass);
+    await balances();
+    ls.setItem("login", ulogin);
+    ls.setItem("password", pass);
   } else if (resp === "помощь") {
     utils.printHelp();
   } else if (resp === "баланс") {
-    let balances = (await user.getUser()).private;
-    if (balances) balances = balances.balances;
-    if (balances) utils.printBal(balances);
-    else console.log(await user.getUser());
+    await balances();
   } else if (resp === "история") {
-    let transactions = (await user.getUser()).private;
-    if (transactions) transactions = transactions.transactions;
-    if (transactions) utils.printTrans(transactions, user.name);
-    else console.log(await user.getUser());
+    await history();
   } else if (resp === "транзакция") {
-    let transaction = await user.transaction(
-      await sr.input("text", "Получатель: "),
-      Number(await sr.input("text", "Количество: ")),
-      await sr.input("text", "Валюта: "),
-      await sr.input("text", "Комментарий: ")
-    );
-    console.log(transaction);
+    await transaction();
   } else {
     utils.printHelp();
   }
   handle();
 }
 
-async function start() {
-  user = new vicopsApi(
-    await sr.input("text", "Введите логин: "),
-    await sr.input("secure", "Введите пароль: ")
-  );
+async function login() {
+  if (ls.getItem("login")) {
+    user = new vicopsApi(ls.getItem("login"), ls.getItem("password"));
+    console.log("Вы вошли автоматически!".green);
+  } else {
+    console.log("Войдите или зарегистрируйтесь (напишите помощь).".red);
+  }
+}
+
+async function balances() {
   let balances = (await user.getUser()).private;
   if (balances) balances = balances.balances;
-  console.log("Активы:");
   if (balances) utils.printBal(balances);
   else console.log(await user.getUser());
+}
+
+async function history() {
+  let transactions = (await user.getUser()).private;
+  if (transactions) transactions = transactions.transactions;
+  if (transactions) utils.printTrans(transactions, user.name);
+  else console.log(await user.getUser());
+}
+
+async function transaction() {
+  let transaction = await user.transaction(
+    await sr.input("text", "Получатель: "),
+    Number(await sr.input("text", "Количество: ")),
+    await sr.input("text", "Валюта: "),
+    await sr.input("text", "Комментарий: ")
+  );
+  console.log(transaction);
 }
