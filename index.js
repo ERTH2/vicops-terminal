3699512c054935692071b948a456e21d248e208c
Artemy <artegoser@gmail.com>
2022-03-06 11:57:04 +0300

    v1.2.0

--- index.js
+++ index.js
@@ -1,51 +1,68 @@
-const colors = require('colors');
+const colors = require("colors");
 const { vicopsApi } = require("vicops-api");
 const { Readline } = require("easy-readline");
 const utils = require("./utils");
 const fs = require("fs");
 let sr = new Readline();
 let user = new vicopsApi();
+
+const CFonts = require("cfonts");
+
+CFonts.say("VICOPS|terminal v1.2.0", {
+  font: "chrome",
+  align: "center",
+  gradient: ["green", "magenta"],
+  transitionGradient: true,
+  env: "node",
+});
+
 handle();
 
-async function handle(){
-    let resp = await sr.input("text", ">>> ");
-    resp = resp.toLowerCase();
+async function handle() {
+  let resp = await sr.input("text", ">>> ");
+  resp = resp.toLowerCase();
 
-    if(resp==="регистрация"){
-        user = new vicopsApi(await sr.input("text", "Введите логин: "), await sr.input("secure", "Введите пароль: "));
-        let response = await user.register(await sr.input("text", "Введите электронную почту для восстановления: "));
-        console.log(response)
-    } else if(resp==="войти"){
-        user = new vicopsApi(await sr.input("text", "Введите логин: "), await sr.input("secure", "Введите пароль: "));
-        let balances = (await user.getUser()).private; if(balances) balances = balances.balances;
-        console.log("Активы:")
-        if(balances) utils.printBal(balances);
-        else console.log(await user.getUser());
-    } else if(resp==="помощь") {
-        utils.printHelp();
-    } else if(resp==="баланс"){
-        let balances = (await user.getUser()).private; if(balances) balances = balances.balances;
-        if(balances) utils.printBal(balances);
-        else console.log(await user.getUser());
-    } else if(resp==="история"){
-        let transactions = (await user.getUser()).private; if(transactions) transactions = transactions.transactions;
-        if(transactions) utils.printTrans(transactions, user.name);
-        else console.log(await user.getUser());
-    } else if(resp==="транзакция"){
-        let transaction = await user.transaction(await sr.input("text", "Получатель: "), Number(await sr.input("text", "Количество: ")), await sr.input("text", "Валюта: "), await sr.input("text", "Комментарий: "));
-        console.log(transaction);
-    } else if(resp==="курс"){
-        let name = await sr.input("text", "Название котировки: ");
-        let course = await user.getCourse(name);
-        if(course.code==="denied") console.log("Котировка не найдена".red);
-        else console.log(`Курс ${name}: ${course.amount}`);
-    } else if(resp==="обмен"){
-        let from = await sr.input("text", "Название валюты с которой менять: ");
-        let to = await sr.input("text", "Название валюты на которую менять: ");
-        let swap = await user.swap(`${from}/${to}`, from, to, Number(await sr.input("text", "Количество единиц валюты для обмена: ")))
-        console.log(swap);
-    } else{
-        utils.printHelp();
-    }
-    handle();
+  if (resp === "регистрация") {
+    user = new vicopsApi(
+      await sr.input("text", "Введите логин: "),
+      await sr.input("secure", "Введите пароль: ")
+    );
+    let response = await user.register(
+      await sr.input("text", "Введите электронную почту для восстановления: ")
+    );
+    console.log(response);
+  } else if (resp === "войти") {
+    user = new vicopsApi(
+      await sr.input("text", "Введите логин: "),
+      await sr.input("secure", "Введите пароль: ")
+    );
+    let balances = (await user.getUser()).private;
+    if (balances) balances = balances.balances;
+    console.log("Активы:");
+    if (balances) utils.printBal(balances);
+    else console.log(await user.getUser());
+  } else if (resp === "помощь") {
+    utils.printHelp();
+  } else if (resp === "баланс") {
+    let balances = (await user.getUser()).private;
+    if (balances) balances = balances.balances;
+    if (balances) utils.printBal(balances);
+    else console.log(await user.getUser());
+  } else if (resp === "история") {
+    let transactions = (await user.getUser()).private;
+    if (transactions) transactions = transactions.transactions;
+    if (transactions) utils.printTrans(transactions, user.name);
+    else console.log(await user.getUser());
+  } else if (resp === "транзакция") {
+    let transaction = await user.transaction(
+      await sr.input("text", "Получатель: "),
+      Number(await sr.input("text", "Количество: ")),
+      await sr.input("text", "Валюта: "),
+      await sr.input("text", "Комментарий: ")
+    );
+    console.log(transaction);
+  } else {
+    utils.printHelp();
+  }
+  handle();
 }
--- utils.js
+++ utils.js
@@ -1,17 +1,44 @@
 module.exports = {
-    printBal:(balances)=>{
-        let curr = Object.keys(balances);
-        let vals = Object.values(balances);
-        for(let i=0; i<curr.length; i++) console.log(`${vals[i]} ${curr[i]}`);
-    },
-    printHelp:()=>{
-        let help = ["регистрация", "войти", "транзакция", "история", "баланс", "обмен", "курс", "помощь"]
-        for(let h of help) console.log(h.rainbow);
-    },
-    printTrans:(transactions, name)=>{
-        for(let transaction of transactions){
-            if(transaction.body.recipient===name) console.log(`${`+ ${transaction.body.amount.toFixed(5)} ${transaction.body.currency}`.green} от ${transaction.body.sender}. Комментарий: ${transaction.body.comment}`);
-            else console.log(`${`- ${transaction.body.amount.toFixed(5)} ${transaction.body.currency}`.red} отправлено ${transaction.body.recipient}. Комментарий: ${transaction.body.comment}`);
-        }
+  printBal: (balances) => {
+    let curr = Object.keys(balances);
+    let vals = Object.values(balances);
+    for (let i = 0; i < curr.length; i++) console.log(`${vals[i]} ${curr[i]}`);
+  },
+  printHelp: () => {
+    let help = [
+      "регистрация",
+      "войти",
+      "транзакция",
+      "история",
+      "баланс",
+      "обмен",
+      "курс",
+      "помощь",
+    ];
+    for (let h of help) console.log(h.blue);
+  },
+  printTrans: (transactions, name) => {
+    for (let transaction of transactions) {
+      if (transaction.body.recipient === name)
+        console.log(
+          `${
+            `+ ${transaction.body.amount.toFixed(5)} ${
+              transaction.body.currency
+            }`.green
+          } от ${transaction.body.sender}. Комментарий: ${
+            transaction.body.comment
+          }`
+        );
+      else
+        console.log(
+          `${
+            `- ${transaction.body.amount.toFixed(5)} ${
+              transaction.body.currency
+            }`.red
+          } отправлено ${transaction.body.recipient}. Комментарий: ${
+            transaction.body.comment
+          }`
+        );
     }
-}
+  },
+};
